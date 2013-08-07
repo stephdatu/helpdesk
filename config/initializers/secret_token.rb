2c0d65c0a72624f1f4a1561ac18df799c12079f2
Stephanie Datu <stephdatu@gmail.com>
2013-08-07 14:08:09 -0700

    Generate Rails4 Helpdesk app and set up gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helpdesk::Application.config.secret_key_base = '8bc68e8a4a408c164f47877eee81abf297921a76be54b5f4e2faa8b55d5cf693ab041d8972d9e425571e658f013c8c5f345baa97b02d8e4ae51890ad00db37b4'
