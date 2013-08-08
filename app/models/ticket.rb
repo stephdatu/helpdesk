a1744ed0c657e9a7ffea7b4a6a6051cc697137a9
Stephanie Datu <stephdatu@gmail.com>
2013-08-08 13:30:54 -0700

    Updating create_from_postmark method

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,7 +5,6 @@ class Ticket < ActiveRecord::Base
     ticket_id = mitt.to.split("@").first.split("+").last
     ticket = Ticket.find_by_ticket_id(ticket_id)
     ticket.message = mitt.text_body
-    ticket.message
     ticket.save
   end
 
