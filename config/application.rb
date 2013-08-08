90a839c8ea495c303e32e08c9e5b6ca76132d4c4
Stephanie Datu <stephdatu@gmail.com>
2013-08-07 17:26:50 -0700

    Set up Postmark for sending emails

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module Helpdesk
   class Application < Rails::Application
+
+    config.action_mailer.delivery_method   = :postmark
+    config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
