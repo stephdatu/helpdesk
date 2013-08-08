f7688e23aa761c1b9de542e1cb9a6f99c4fe5a93
Stephanie Datu <stephdatu@gmail.com>
2013-08-08 15:12:26 -0700

    Fixed id error in api/tickets_controller

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,7 +3,7 @@ class Ticket < ActiveRecord::Base
 
   def self.create_from_postmark(mitt)
     ticket_id = mitt.to.split("@").first.split("+").last
-    ticket = Ticket.find(ticket.id)
+    ticket = Ticket.find(ticket_id)
     ticket.message = mitt.text_body
     ticket.save
   end
