c064fb3804e885544be18dd071d78a7ca9895776
Stephanie Datu <stephdatu@gmail.com>
2013-08-08 11:48:00 -0700

    Still testing for production in Heroku

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,15 +2,15 @@ class Ticket < ActiveRecord::Base
   validates :name, :subject, presence: true
 
   def self.create_from_postmark(mitt)
-        ticket_id = mitt.to.split("@").first.split("+").last
+    ticket_id = mitt.to.split("@").first.split("+").last
 
-        ticket = Ticket.find_by_ticket_id(ticket_id)
-        ticket.message_id = mitt.message_id
-        ticket.title = mitt.subject
-        ticket.body = if mitt.text_body.blank?
-        mitt.text_body
-
-        ticket.save
+    ticket = Ticket.find_by_ticket_id(ticket_id)
+    ticket.message_id = mitt.message_id
+    ticket.title = mitt.subject
+    ticket.body = if mitt.text_body.blank?
+      mitt.text_body
+    end
+    ticket.save
   end
 
 end
